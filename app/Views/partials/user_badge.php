56021a27748acbf7a757ecd5dc469300f5749885
Santi <santiago.gonzalez.lago@gmail.com>
2025-03-21 14:19:53 +0100

    Tooltip en jugadores nuevos

--- app/Views/partials/user_badge.php
+++ app/Views/partials/user_badge.php
@@ -1,4 +1,4 @@
-<a href="<?= base_url('character') ?>/<?= $player->character_uid ?>" class="btn btn-<?= $player->badge_color ?> badge badge-player <?= $player->priority ? "border-priority" : "" ?>">
+<a href="<?= base_url('character') ?>/<?= $player->character_uid ?>" class="btn btn-<?= $player->badge_color ?> badge badge-player <?= $player->priority ? "border-priority" : "" ?>" <?= $player->priority ? 'data-toggle="tooltip" title="Los jugadores nuevos tienen prioridad"' : '' ?>>
     <div><?= $player->name ?></div>
     <div>(<?= $player->display_name ?>)</div>
 </a>
