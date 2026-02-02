9a05c1c2af94cfb0c7afb582da2292b099310780
Santi <santiago.gonzalez.lago@gmail.com>
2026-02-02 18:48:22 +0100

    fix galletitas 🍪

--- app/Views/cookies.php
+++ app/Views/cookies.php
@@ -18,7 +18,7 @@
 
 <h1 class="mb-4 text-center">Política de Cookies</h1>
 
-<p>Este sitio web utiliza cookies propias y de terceros para ofrecerle una mejor experiencia y servicio. Al navegar o utilizar nuestros servicios el usuario acepta el uso que hacemos de las cookies. Sin embargo, el usuario tiene la opción de impedir la generación de cookies y la eliminación de las mismas mediante la selección de la correspondiente opción en su navegador. En caso de bloquear el uso de cookies en su navegador es posible que algunos servicios o funcionalidades de la página web no estén disponibles.</p>
+<p>Este sitio web utiliza cookies propias y de terceros para ofrecerle una mejor experiencia y servicio. Este sitio web utiliza únicamente cookies funcionales necesarias para su correcto funcionamiento. Estas cookies no requieren el consentimiento del usuario. Sin embargo, el usuario tiene la opción de impedir la generación de cookies y la eliminación de las mismas mediante la selección de la correspondiente opción en su navegador. En caso de bloquear el uso de cookies en su navegador es posible que algunos servicios o funcionalidades de la página web no estén disponibles.</p>
 
 <h2 class="mt-4">¿Qué es una cookie?</h2>
 <p>Una cookie es un pequeño fragmento de texto que los sitios web envían al navegador y que se almacenan en el terminal del usuario, el cual puede ser un ordenador personal, un teléfono móvil, una tableta, etc. Estos archivos permiten que el sitio web recuerde información sobre su visita, como el idioma y las opciones preferidas, lo que puede facilitar su próxima visita y hacer que el sitio resulte más útil al personalizar su contenido. Las cookies desempeñan un papel muy importante, al mejorar la experiencia del uso de la web.</p>
@@ -26,11 +26,7 @@
 <h2 class="mt-4">¿Cómo se utilizan las cookies?</h2>
 <p>Al navegar por este portal web el usuario está aceptando que se puedan instalar cookies en su terminal y que nos permiten conocer la siguiente información:</p>
 <ul>
-  <li>Información estadística del uso de la web.</li>
   <li>El login del usuario para mantener la sesión activa en la web.</li>
-  <li>El formato de la web preferente en el acceso desde dispositivos móviles.</li>
-  <li>Las últimas búsquedas realizadas en los servicios de la web y datos de personalización de estos servicios.</li>
-  <li>Información de las encuestas en las que ha participado el usuario.</li>
   <li>Datos de la conexión con las redes sociales para los usuarios que acceden con su cuenta de Google.</li>
 </ul>
 
